b9de2e74949b53cc64874779cd1c1aa7f6da8c79
saadhzubairi <saadhzubairi@outlook.com>
2023-06-05 14:25:09 +0500

    top bar changes

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -9,22 +9,7 @@ function TopBar() {
                     <span className="logo">Hoops</span>
                 </Link>
             </div>
-            <div className="topBarCentre">
-                <div className="searchbar">
-                    <Search className="SearchIcon" />
-                    <input placeholder="Search stuff" type="text" className="searchInput" />
-                </div>
-            </div>
             <div className="topBarRight">
-                <div className="topBarLinks">
-                    <span className="topBarLink">Homepage</span>
-                    <span className="topBarLink">Timeline</span>
-                </div>
-                <div className="topBarIcons">
-                    <div className="topBarIconItem"><Person /><span className="topBarIconBadge">1</span></div>
-                    <div className="topBarIconItem"><Chat /><span className="topBarIconBadge">1</span></div>
-                    <div className="topBarIconItem"><Notifications /><span className="topBarIconBadge">1</span></div>
-                </div>
                 <img className="topBarImage" src="\assets\person\2.jpeg" alt="" />
             </div>
         </div>
